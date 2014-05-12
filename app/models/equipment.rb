3b89fa2dafda18a89818b081aa8a2eb1fe7c48c6
Buddy Magsipoc <keikun17@gmail.com>
2014-05-12 20:38:51 +0800

    Annotate。

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,2 +1,18 @@
 class Equipment < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: equipment
+#
+#  id           :integer          not null, primary key
+#  description  :text
+#  listed_specs :hstore
+#  actual_specs :hstore
+#  itemcode     :string(255)
+#  installed_at :date
+#  replaced_at  :date
+#  failure_mode :date
+#  created_at   :datetime
+#  updated_at   :datetime
+#
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,14 @@
 class Property < ActiveRecord::Base
   belongs_to :user
 end
+
+# == Schema Information
+#
+# Table name: properties
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
