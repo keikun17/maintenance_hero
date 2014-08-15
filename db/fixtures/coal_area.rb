1828a8c2ec20e201ca6721681a1f66d10c9e2ffd
Buddy Magsipoc <keikun17@gmail.com>
2014-08-15 13:38:47 +0800

    Rewrite seed。

--- db/fixtures/coal_area.rb
+++ db/fixtures/coal_area.rb
@@ -1,11 +1,11 @@
 # Passing in :name here as a constraint. Read more about seed-fu constrains here
 # https://github.com/mbleigh/seed-fu
 Location.seed(:name) do |s|
-  s.name = "Raw mill"
+  s.name = "Raw mill L2A-BC1"
 end
 
-location_id = Location.find_by(name: "Raw mill").id
+location_id = Location.find_by(name: "Raw mill L2A-BC1").id
 
 System.seed(:name, :location_id) do |s|
-  s.name = "Conveyor"
+  s.name = "Conveying System"
 end
