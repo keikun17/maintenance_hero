3d1c0f3d2efec91acb9f7753f50bd7e422559e89
Buddy Magsipoc <keikun17@gmail.com>
2014-08-15 16:17:55 +0800

    Equipment seeds。

--- db/fixtures/coal_area.rb
+++ db/fixtures/coal_area.rb
@@ -1,11 +1,32 @@
-# Passing in :name here as a constraint. Read more about seed-fu constrains here
-# https://github.com/mbleigh/seed-fu
 Location.seed(:name) do |s|
   s.name = "Raw mill L2A-BC1"
 end
 
 location_id = Location.find_by(name: "Raw mill L2A-BC1").id
 
+# Systems
+
 System.seed(:name, :location_id) do |s|
   s.name = "Conveying System"
 end
+
+system_id = System.find_by(name: 'Conveying System')
+
+# Equipments
+
+Equipment.seed(:name, :system_id) do |s|
+  s.name = 'Conveyor Belt'
+end
+
+Equipment.seed(:name, :system_id) do |s|
+  s.name = 'Belt Cleaner'
+end
+
+Equipment.seed(:name, :system_id) do |s|
+  s.name = 'Head Pulley'
+end
+
+Equipment.seed(:name, :system_id) do |s|
+  s.name = 'Idler Rollers'
+end
+
--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-location_names = %w(L2A-BC1 Crusher Coal)
-
-location_names.each do |location_name|
-  Location.find_or_create_by(name: location_name)
-end
