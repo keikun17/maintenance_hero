50544d96625950de9a67bef3507c7543cac70d3f
Buddy Magsipoc <keikun17@gmail.com>
2014-08-15 16:48:31 +0800

    Seed fuckup。

--- db/fixtures/coal_area.rb
+++ db/fixtures/coal_area.rb
@@ -8,25 +8,30 @@ location_id = Location.find_by(name: "Raw mill L2A-BC1").id
 
 System.seed(:name, :location_id) do |s|
   s.name = "Conveying System"
+  s.location_id = location_id
 end
 
-system_id = System.find_by(name: 'Conveying System')
+system_id = System.find_by(name: 'Conveying System').id
 
 # Equipments
 
 Equipment.seed(:name, :system_id) do |s|
   s.name = 'Conveyor Belt'
+  s.system_id = system_id
 end
 
 Equipment.seed(:name, :system_id) do |s|
   s.name = 'Belt Cleaner'
+  s.system_id = system_id
 end
 
 Equipment.seed(:name, :system_id) do |s|
   s.name = 'Head Pulley'
+  s.system_id = system_id
 end
 
 Equipment.seed(:name, :system_id) do |s|
   s.name = 'Idler Rollers'
+  s.system_id = system_id
 end
 
