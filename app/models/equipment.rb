d301976da86a14023676a225417a2ffe4acb9a7b
Buddy Magsipoc <keikun17@gmail.com>
2014-08-16 19:34:24 +0800

    Validation。

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,4 @@
 class Equipment < ActiveRecord::Base
   belongs_to :system
+  validates :system_id, presence: true
 end
--- app/models/system.rb
+++ app/models/system.rb
@@ -1,4 +1,6 @@
 class System < ActiveRecord::Base
   belongs_to :location
   has_many :equipments
+
+  validates :location_id, presence: true
 end
